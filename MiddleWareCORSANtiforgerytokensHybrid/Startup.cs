4d605b22f58551afbb738d2626bcde37cb1a03e6
tarin <clement.tarin@gmail.com>
2025-05-16 14:23:17 +0200

    "WebApiConfig, CORS"

--- MiddleWareCORSANtiforgerytokensHybrid/Startup.cs
+++ MiddleWareCORSANtiforgerytokensHybrid/Startup.cs
@@ -20,7 +20,7 @@ using System.ServiceModel.Configuration;
 using Microsoft.Owin.Security.Jwt;
 using Microsoft.IdentityModel.Tokens;
 using System.ServiceModel.Security.Tokens;
-
+using AuthenticationMode = Microsoft.Owin.Security.AuthenticationMode;
 [assembly: OwinStartup(typeof(Startup))]
 public class Startup
 {
@@ -36,10 +36,6 @@ public class Startup
     {
         AuthenticationMode = AuthenticationMode.Active,
         AllowedAudiences = new[] { audience },
-        IssuerSecurityTokenProviders = new IIssuerSecurityTokenProvider[]
-        {
-            new SymmetricKeyIssuerSecurityTokenProvider(issuer, secretBytes)
-        },
         TokenValidationParameters = new TokenValidationParameters
         {
             ValidateIssuer = true,
@@ -56,11 +52,47 @@ public class Startup
         app.UseWebApi(config);
     }
 
-    private class WebApiConfig
+    internal static class WebApiConfig
     {
-        internal static void Register(HttpConfiguration config)
+        public static void Register(HttpConfiguration config)
         {
-            throw new NotImplementedException();
+            // Enable CORS (configure appropriately for production)
+            var cors = new EnableCorsAttribute(
+                origins: "*",
+                headers: "*",
+                methods: "*"
+            );
+            config.EnableCors(cors);
+
+            // Configure attribute routing
+            config.MapHttpAttributeRoutes();
+
+            // Convention-based routing
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            // Configure JSON formatter
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting =
+                Newtonsoft.Json.Formatting.Indented;
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver =
+                new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver();
+
+            // Remove XML formatter (optional)
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            // Configure error handling
+            config.IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.LocalOnly;
+
+            // Enable HTTPS in production
+#if !DEBUG
+            config.Filters.Add(new RequireHttpsAttribute());
+#endif
+
+            // Add message handlers (like your AntiForgeryTokenHandler)
+            config.MessageHandlers.Add(new JwtAntiForgeryTokenHandler());
         }
     }
 }
